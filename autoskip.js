c9d93a0400816e69b1c18a4d64a3eaaf87b02ee0
Jeff <jeffstahlin@gmail.com>
2018-11-17 22:50:19 +0100

    works for amazon prime now :-)

--- autoskip.js
+++ autoskip.js
@@ -1,4 +1,3 @@
-
 // Send each DOM mutation through a filtering function.
 const obs = new MutationObserver(mutations => mutations.map(matchAddedNodes));
 
@@ -7,12 +6,11 @@ matchAddedNodes = mutation => mutation.addedNodes.forEach(matchAndClick);
 
 // Try matching a node for the "skip" button and click on positive match.
 matchAndClick = node => {
-  if (node.nodeType === 1 && node.matches('.skip-credits'))
-    click(node)
-}
+  if (node.nodeType === 1 && node.matches(".skipElement")) click(node);
+};
 
 // Click the node.
-click = node => node.firstChild.click();
+click = node => node.click();
 
 // Observe the DOM for changes.
 obs.observe(document.documentElement, { childList: true, subtree: true });
