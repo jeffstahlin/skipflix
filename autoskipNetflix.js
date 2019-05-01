bacbe9fb5eaf53ad860bd086a161254b6b2e930b
Jeff <jeffstahlin@gmail.com>
2019-05-02 01:48:48 +0200

    add netflix support as well

---
+++ autoskipNetflix.js
@@ -0,0 +1,16 @@
+// Send each DOM mutation through a filtering function.
+const obs = new MutationObserver(mutations => mutations.map(matchAddedNodes));
+
+// Send each added node through the matching and clicking function.
+matchAddedNodes = mutation => mutation.addedNodes.forEach(matchAndClick);
+
+// Try matching a node for the "skip" button and click on positive match.
+matchAndClick = node => {
+  if (node.nodeType === 1 && node.matches(".skip-credits")) click(node);
+};
+
+// Click the node.
+click = node => node.firstChild.click();
+
+// Observe the DOM for changes.
+obs.observe(document.documentElement, { childList: true, subtree: true });
